668a59fe883291b0a18a9ef58ad6529126954f74
Raul Pontello <62081176+RaulPontello@users.noreply.github.com>
2026-04-08 16:40:58 -0300

    Delete locals.tf

--- locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  generic_prefix        = "${var.project_name}-${var.environment}"
-  bucket_name           = "${local.generic_prefix}-s3-bucket"
-  kinesis_stream_name   = "${local.generic_prefix}-kinesis-stream"
-  kinesis_firehose_name = "${local.generic_prefix}-kinesis-firehose"
-  firehose_prefix       = "stock-data/year=!{timestamp:yyyy}/month=!{timestamp:MM}/day=!{timestamp:dd}/hour=!{timestamp:HH}/"
-  firehose_error_prefix = "errors/!{firehose:error-output-type}/year=!{timestamp:yyyy}/month=!{timestamp:MM}/day=!{timestamp:dd}/"
-
-}
